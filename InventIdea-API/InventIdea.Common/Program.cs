858031baed858ccb0834e561c5729a207879baec
marchiani <marchiani00@gmail.com>
2020-01-12 21:48:49 +0300

    Replace core projects on .net projects Common Data Service

--- InventIdea-API/InventIdea.Common/Program.cs
+++ InventIdea-API/InventIdea.Common/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace InventIdea.Common
 {
@@ -6,7 +10,6 @@ namespace InventIdea.Common
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
--- InventIdea-API/InventIdea.Services/Program.cs
+++ InventIdea-API/InventIdea.Services/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace InventIdea.Services
 {
@@ -6,7 +10,6 @@ namespace InventIdea.Services
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
